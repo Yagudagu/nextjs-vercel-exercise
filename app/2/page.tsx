ecbf5c85dc83d61cc3567ac7e223ac088438e447
Jacob Martin <Jacobhmartian@gmail.com>
2023-11-07 09:08:54 -0700

    let's try again

--- app/2/page.tsx
+++ app/2/page.tsx
@@ -1,9 +1,15 @@
 export default function Page() {
   return (
-    <h1>
-      2. How would you compare Next.js with another framework? Feel free to
-      compare with a framework of your choice.
-    </h1>
-    <p>Next.js is a server side rendering framework for React. Doing the rendering work on the server side saves processing power and network bandwidth on the client side. </p>
+    <>
+      <h1>
+        2. How would you compare Next.js with another framework? Feel free to
+        compare with a framework of your choice.
+      </h1>
+      <p>
+        Next.js is a server side rendering framework for React. Doing the
+        rendering work on the server side saves processing power and network
+        bandwidth on the client side.{" "}
+      </p>
+    </>
   );
 }
--- app/4/page.tsx
+++ app/4/page.tsx
@@ -1,21 +1,19 @@
 export default function Page() {
   return (
-    <h1>
-      4. A customer has a project on Vercel and they want to redirect the /blog
-      path to another website. Please write a reply to the customer. Separately,
-      list any relevant documentation you found and any feedback or information
-      you’d like to share about your decision making process.
-    </h1>
+    <>
+      <h1>
+        4. A customer has a project on Vercel and they want to redirect the
+        /blog path to another website. Please write a reply to the customer.
+        Separately, list any relevant documentation you found and any feedback
+        or information you’d like to share about your decision making process.
+      </h1>
 
-    <p>
-      Hi,
-
-      Redirecting on Vercel is easy! Please take a look at this document below. 
-      https://vercel.com/docs/edge-network/redirects
-
-      
-
-      Static redirects would be the same for everyone and dynamic redirects are an option if you need to change the redirects based on the interaction with the site.
-    </p>
+      <p>
+        Hi, Redirecting on Vercel is easy! Please take a look at this document
+        below. https://vercel.com/docs/edge-network/redirects Static redirects
+        would be the same for everyone and dynamic redirects are an option if
+        you need to change the redirects based on the interaction with the site.
+      </p>
+    </>
   );
 }
