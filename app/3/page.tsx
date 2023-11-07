1d769c7f972148377ba1a70edd0ed4aeb58359d2
Jacob Martin <Jacobhmartian@gmail.com>
2023-11-07 09:04:13 -0700

    again

--- app/3/page.tsx
+++ app/3/page.tsx
@@ -4,8 +4,6 @@ export default function Page() {
       3. When would you choose to use Edge Functions, Serverless Functions, or
       Edge Middleware with Vercel?
     </h1>
-    <p>Serverless functions are great for a personal project, but Edge functions are a better choice if the highest level of performance is required. 
-      
-       Edge middleware is a way to affect a request before it gets to the Edge cache, so it's a great way to provide personalization without sacraficing performance.</p>
+    <p>Serverless functions are great for a personal project, but Edge functions are a better choice if the highest level of performance is required. Edge middleware is a way to affect a request before it gets to the Edge cache, so it's a great way to provide personalization without sacraficing performance.</p>
   );
 }
