290a2aec04192ee1d6f2827976b1f5c528375294
Jacob Martin <Jacobhmartian@gmail.com>
2023-11-07 10:19:44 -0700

    ul issue

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,21 @@ export default function Home() {
         <h1 className="text-2x1">Todos</h1>
       </header>{" "}
       <ul className="pl-4">
-        <Link href="/1">Question One</Link>
-        <Link href="/2">Question Two</Link>
-        <Link href="/3">Question Three</Link>
-        <Link href="/4">Question Four</Link>
-        <Link href="/5">Question Five</Link>
+        <li>
+          <Link href="/1">Question One</Link>
+        </li>
+        <li>
+          <Link href="/2">Question Two</Link>
+        </li>
+        <li>
+          <Link href="/3">Question Three</Link>
+        </li>
+        <li>
+          <Link href="/4">Question Four</Link>
+        </li>
+        <li>
+          <Link href="/5">Question Five</Link>
+        </li>
       </ul>
     </>
   );
